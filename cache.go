ee629881371fdb1ee05f2bd6d4bdfef03a0a79cf
RavshanovUsmonbek <usmonravshanov@list.ru>
2022-05-23 15:17:39 +0500

    Initial try

--- cache.go
+++ cache.go
@@ -1,23 +1,44 @@
 package cache
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type Cache struct {
+	data map[string]string
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{data: map[string]string{}}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c *Cache) Get(key string) (string, bool) {
+	value, ok := c.data[key]
+	return value, ok
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.data[key] = value
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	keys := []string{}
+	for key := range c.data {
+		keys = append(keys, key)
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func removeFromMap(dict map[string]string, key string) func() {
+	return func() {
+		delete(dict, key)
+	}
+}
+
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	duration := deadline.Sub(time.Now())
+	fmt.Println("Duration:", duration)
+	time.AfterFunc(duration, removeFromMap(c.data, key))
+	c.data[key] = value
 }
